cc8d47d2dd16774f249bc3e128b9ee36a6543c94
rooney <bch3454@gmail.com>
2023-05-23 00:25:05 +0900

    Feat: TabNav 스타일

--- navigations/Tabs.js
+++ navigations/Tabs.js
@@ -2,12 +2,35 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
-
+import { useColorScheme } from "react-native";
+import {
+  BLACK_COLOR,
+  DARK_GRAY_COLOR,
+  GRAY_COLOR,
+  WHITE_COLOR,
+  YELLOW_COLOR,
+} from "../colors";
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
+  const isDark = useColorScheme() === "dark";
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : WHITE_COLOR,
+        },
+        tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR,
+        tabBarInactiveTintColor: isDark ? GRAY_COLOR : DARK_GRAY_COLOR,
+        headerStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : WHITE_COLOR,
+        },
+        headerTitleStyle: {
+          color: isDark ? YELLOW_COLOR : BLACK_COLOR,
+        },
+      }}
+    >
       <Tab.Screen name="Movies" component={Movies} />
       <Tab.Screen name="Tv" component={Tv} />
       <Tab.Screen name="Search" component={Search} />
