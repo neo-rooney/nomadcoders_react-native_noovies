407445578bc14c4f99b1aef366dd106513654aa2
rooney <bch3454@gmail.com>
2023-05-25 23:58:58 +0900

    Feat: Styped-component 적용

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,16 +1,21 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
 import { Text, View, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+
+const StyledTouchableOpacity = styled.TouchableOpacity`
+  background-color: papayawhip;
+`;
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
   navigation: { navigate },
 }) => (
-  <TouchableOpacity
+  <StyledTouchableOpacity
     onPress={() => navigate("Stack", { screen: "screenOne" })}
     style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
   >
     <Text>Movies</Text>
-  </TouchableOpacity>
+  </StyledTouchableOpacity>
 );
 
 export default Movies;
