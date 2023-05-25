5c5bcb3d9d4645d3f032ae5cece19019dc1e2b06
rooney <bch3454@gmail.com>
2023-05-25 23:49:55 +0900

    Feat: Typescript 적용

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,7 +1,10 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
 import { Text, View, TouchableOpacity } from "react-native";
 
-const Movies = ({ navigation: { navigate } }) => (
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
+  navigation: { navigate },
+}) => (
   <TouchableOpacity
     onPress={() => navigate("Stack", { screen: "screenOne" })}
     style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
